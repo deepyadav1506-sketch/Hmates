64722a481287cf3d7ecb545d126569423ddf293d
Ayush Bishnoi <ayushbishnoi900@gmail.com>
2026-03-16 19:03:01 +0530

    new chnges in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const path = require("path");
+const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
-const filePath = path.join(__dirname, "../users.json");
+
 
 /* REGISTER */
 router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  if (!username || !email || !password) {
-    return res.status(400).json({ message: "All fields required" });
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully ✅" });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
   }
-
-  let users = JSON.parse(fs.readFileSync(filePath));
-
-  const existingUser = users.find(user => user.email === email);
-
-  if (existingUser) {
-    return res.status(400).json({ message: "Email already exists" });
-  }
+});
 
   // 🔐 HASH PASSWORD
   const hashedPassword = await bcrypt.hash(password, 10);
